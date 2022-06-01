15553599c9353608d79743f302c1728a20d79a00
MarcosTypeAP <marcosarielpaccor@gmail.com>
2022-06-01 10:43:55 -0300

    Added code tables

---
+++ hid_key_codes.py
@@ -0,0 +1,111 @@
+HID_KEY_CODES = {
+    'esc': 0x29,
+    'f1': 0x3A,
+    'f2': 0x3B,
+    'f3': 0x3C,
+    'f4': 0x3D,
+    'f5': 0x3E,
+    'f6': 0x3F,
+    'f7': 0x40,
+    'f8': 0x41,
+    'f9': 0x42,
+    'f10': 0x43,
+    'f11': 0x44,
+    'f12': 0x45,
+    'imp pant': 0x46,
+    'print screen': 0x46,  # imp pant
+    'bloq despl': 0x47,
+    'scroll lock': 0x47,  # bloq despl
+    'pausa': 0x48,
+    'pause': 0x48,  # pausa
+    '|': 0x31,  # hmmm
+    '1': 0x1E,
+    '2': 0x1F,
+    '3': 0x20,
+    '4': 0x21,
+    '5': 0x22,
+    '6': 0x23,
+    '7': 0x24,
+    '8': 0x25,
+    '9': 0x26,
+    '0': 0x27,
+    '=': 0x2E,  # hmmm
+    "'": 0x34,
+    '¿': 0x38,  # hmmm
+    'backspace': 0x2A,
+    'insert': 0x49,
+    'inicio': 0x4A,
+    'home': 0x49,  # inicio
+    're pag': 0x4B,
+    'page up': 0x4B,  # re pag
+    'tab': 0x2B,
+    'a': 0x04,
+    'b': 0x05,
+    'c': 0x06,
+    'd': 0x07,
+    'e': 0x08,
+    'f': 0x09,
+    'g': 0x0A,
+    'h': 0x0B,
+    'i': 0x0C,
+    'j': 0x0D,
+    'k': 0x0E,
+    'l': 0x0F,
+    'm': 0x10,
+    'n': 0x11,
+    'o': 0x12,
+    'p': 0x13,
+    'q': 0x14,
+    'r': 0x15,
+    's': 0x16,
+    't': 0x17,
+    'u': 0x18,
+    'v': 0x19,
+    'w': 0x1A,
+    'x': 0x1B,
+    'y': 0x1C,
+    'z': 0x1D,
+    '[': 0x2F,  # hmmm
+    ']': 0x30,  # hmmm
+    '´': None,
+    '´+': 0x2E,  # hmmm
+    'enter': 0x28,
+    'supr': 0x4C,
+    'delete': 0x4C,  # supr
+    'fin': 0x4D,
+    'end': 0x4D,  # fin
+    'av pag': 0x4E,
+    'page down': 0x4E,  # av pag
+    'bloq mayus': 0x39,
+    'caps lock': 0x39,  # bloq mayus
+    ';': 0x33,
+    '\\': 0x35,  # hmmmmmmmm
+    'shift': 0xE1,  # mayusculas
+    '{': 0x2F,  # hmmm
+    '}': 0x30,  # hmmm
+    'mayusculas': 0xE1,
+    '<': 0x36,  # hmmm
+    ',': 0x36,
+    '.': 0x37,
+    '/': 0x38,  # hmmm
+    '-': 0x2D,
+    'right shift': 0xE1,  # mayusculas
+    'ctrl': 0xE0,
+    'windows izquierda': 0xE3,
+    'left windows': 0xE3,  # windows izquierda
+    'alt': 0xE2,
+    'right alt': 0xE2,  # alt
+    'space': 0x2C,
+    'aplicación': 0x65,
+    'menu': 0x65,  # aplicación
+    'ctrl derecha': 0xE0,  # ctrl
+    'right ctrl': 0xE0,  # ctrl
+    'flecha izquierda': 0x50,
+    'left': 0x50,  # flecha izquierda
+    'flecha abajo': 0x51,
+    'down': 0x51,  # flecha abajo
+    'flecha derecha': 0x4F,
+    'right': 0x4F,  # flecha derecha
+    'flecha arriba': 0x52,
+    'up': 0x52,  # flecha arriba
+}
---
+++ map-keys.py
@@ -0,0 +1,11 @@
+def main():
+    with open('raw-file.txt', 'r') as fp:
+        file_lines = fp.readlines()
+
+    new_file_lines = []
+    for file_line in file_lines:
+        new_file_line = ''
+
+
+if __name__ == '__main__':
+    main()
